9fbf68f0abfc51687f01d589b56563144b0f260f
Hernandsv01 <hernanxdsv@gmail.com>
2022-05-22 19:24:58 -0300

    Fixed code format

--- TDA ABB/pruebas.c
+++ TDA ABB/pruebas.c
@@ -5,65 +5,74 @@
 
 #define TAMANIO_VECTOR_PRUEBA 6
 
-typedef struct cosa{
+typedef struct cosa
+{
   int clave;
   char contenido[10];
-}cosa_t;
+} cosa_t;
 
-cosa_t *crear_cosa(int clave){
+cosa_t *crear_cosa(int clave)
+{
   cosa_t *cosa = (cosa_t *)malloc(sizeof(cosa_t));
-  if(cosa)
+  if (cosa)
     cosa->clave = clave;
   return cosa;
 }
 
-int comparador(void *elemento_uno, void *elemento_dos){
-  if(!elemento_uno || !elemento_dos) return 0;
+int comparador(void *elemento_uno, void *elemento_dos)
+{
+  if (!elemento_uno || !elemento_dos)
+    return 0;
   cosa_t *uno = elemento_uno;
   cosa_t *dos = elemento_dos;
   return uno->clave - dos->clave;
 }
 
-void destruir_cosa(cosa_t *cosa){
+void destruir_cosa(cosa_t *cosa)
+{
   free(cosa);
 }
 
-void destructor(void *c){
+void destructor(void *c)
+{
   cosa_t *cosa = c;
-  if(cosa)
-  destruir_cosa(c);
+  if (cosa)
+    destruir_cosa(c);
 }
 
-//PRUEBAS CON NULL
-void pruebas_arbol_null(){
+// PRUEBAS CON NULL
+void pruebas_arbol_null()
+{
   pa2m_afirmar(abb_crear(NULL) == NULL, "Al intentar crear un abb con funcion comparador nula, retorna NULL.");
   void *elemento = (void *)1;
-  pa2m_afirmar(abb_insertar(NULL, elemento) ==NULL, "Insertar un elemento en un abb nulo retorna NULL.");
-  pa2m_afirmar(abb_quitar(NULL, elemento)==NULL, "Quitar un elemento de un abb nulo retorna NULL.");
-  pa2m_afirmar(abb_buscar(NULL, elemento)==NULL, "Buscar un elemento de un abb nulo retorna NULL.");
-  pa2m_afirmar(abb_vacio(NULL)== true, "Un arbol nulo esta vacio.");
-  pa2m_afirmar(abb_tamanio(NULL) == 0,"El tamanio de un abb nulo es 0.");
-  pa2m_afirmar(abb_con_cada_elemento(NULL, INORDEN, false, NULL)== 0, "Recorrer los elementos (iterador interno) INORDEN de un arbol nulo retorna 0.");
-  pa2m_afirmar(abb_con_cada_elemento(NULL, POSTORDEN, false, NULL)== 0, "Recorrer los elementos (iterador interno) POSTORDEN de un arbol nulo retorna 0.");
-  pa2m_afirmar(abb_con_cada_elemento(NULL, PREORDEN, false, NULL)== 0, "Recorrer los elementos (iterador interno) PREORDEN de un arbol nulo retorna 0.");
-  pa2m_afirmar(abb_recorrer(NULL, INORDEN, NULL, 0)== 0,"Recorrer los elementos con recorrido INORDEN de un arbol nulo retorna 0 elementos recorridos.");
-  pa2m_afirmar(abb_recorrer(NULL, PREORDEN, NULL, 0)== 0,"Recorrer los elementos con recorrido PREORDEN de un arbol nulo retorna 0 elementos recorridos.");
-  pa2m_afirmar(abb_recorrer(NULL, POSTORDEN, NULL, 0)== 0,"Recorrer los elementos con recorrido POSTORDEN de un arbol nulo retorna 0 elementos recorridos.");
+  pa2m_afirmar(abb_insertar(NULL, elemento) == NULL, "Insertar un elemento en un abb nulo retorna NULL.");
+  pa2m_afirmar(abb_quitar(NULL, elemento) == NULL, "Quitar un elemento de un abb nulo retorna NULL.");
+  pa2m_afirmar(abb_buscar(NULL, elemento) == NULL, "Buscar un elemento de un abb nulo retorna NULL.");
+  pa2m_afirmar(abb_vacio(NULL) == true, "Un arbol nulo esta vacio.");
+  pa2m_afirmar(abb_tamanio(NULL) == 0, "El tamanio de un abb nulo es 0.");
+  pa2m_afirmar(abb_con_cada_elemento(NULL, INORDEN, false, NULL) == 0, "Recorrer los elementos (iterador interno) INORDEN de un arbol nulo retorna 0.");
+  pa2m_afirmar(abb_con_cada_elemento(NULL, POSTORDEN, false, NULL) == 0, "Recorrer los elementos (iterador interno) POSTORDEN de un arbol nulo retorna 0.");
+  pa2m_afirmar(abb_con_cada_elemento(NULL, PREORDEN, false, NULL) == 0, "Recorrer los elementos (iterador interno) PREORDEN de un arbol nulo retorna 0.");
+  pa2m_afirmar(abb_recorrer(NULL, INORDEN, NULL, 0) == 0, "Recorrer los elementos con recorrido INORDEN de un arbol nulo retorna 0 elementos recorridos.");
+  pa2m_afirmar(abb_recorrer(NULL, PREORDEN, NULL, 0) == 0, "Recorrer los elementos con recorrido PREORDEN de un arbol nulo retorna 0 elementos recorridos.");
+  pa2m_afirmar(abb_recorrer(NULL, POSTORDEN, NULL, 0) == 0, "Recorrer los elementos con recorrido POSTORDEN de un arbol nulo retorna 0 elementos recorridos.");
 }
 
-//creacion
-void se_crea_un_arbol_exitosamente(){
+// creacion
+void se_crea_un_arbol_exitosamente()
+{
   abb_t *arbol = abb_crear(comparador);
   pa2m_afirmar(arbol != NULL, "Se crea un arbol correctamente.");
   pa2m_afirmar(arbol->comparador == comparador, "El comparador del arbol es el correspondiente.");
-  pa2m_afirmar(arbol->tamanio == 0 , "El tamanio del arbol es 0.");
+  pa2m_afirmar(arbol->tamanio == 0, "El tamanio del arbol es 0.");
   pa2m_afirmar(abb_vacio(arbol), "El arbol recien creado sin elementos esta vacio.");
   pa2m_afirmar(arbol->nodo_raiz == NULL, "El nodo raiz de un abb recien creado apunta a NULL.");
   abb_destruir(arbol);
 }
 
-//insercion 
-void se_inserta_un_elemento_en_arbol_obtengo_arbol_con_un_elemento(){
+// insercion
+void se_inserta_un_elemento_en_arbol_obtengo_arbol_con_un_elemento()
+{
   abb_t *arbol = abb_crear(comparador);
   cosa_t *cosa = crear_cosa(1);
   pa2m_afirmar(arbol != NULL, "Se crea un arbol correctamente.");
@@ -74,14 +83,15 @@ void se_inserta_un_elemento_en_arbol_obtengo_arbol_con_un_elemento(){
   abb_destruir_todo(arbol, destructor);
 }
 
-void se_insertan_elementos_en_arbol_obtengo_arbol_con_elementos(){
+void se_insertan_elementos_en_arbol_obtengo_arbol_con_elementos()
+{
   abb_t *arbol = abb_crear(comparador);
   cosa_t *cosa_uno = crear_cosa(5);
   cosa_t *cosa_dos = crear_cosa(2);
   cosa_t *cosa_tres = crear_cosa(7);
   cosa_t *cosa_cuatro = crear_cosa(1);
   cosa_t *auxiliar = crear_cosa(0);
-  pa2m_afirmar(arbol!= NULL, "Se crea un arbol exitosamente.");
+  pa2m_afirmar(arbol != NULL, "Se crea un arbol exitosamente.");
   pa2m_afirmar(arbol->nodo_raiz == NULL, "El nodo raiz de arbol recien creado apunta a null.");
   pa2m_afirmar(abb_vacio(arbol), "Un arbol recien creado se encuentra vacio.");
   pa2m_afirmar(abb_insertar(arbol, cosa_uno), "Se inserta un elemento al arbol exitosamente.");
@@ -93,21 +103,22 @@ void se_insertan_elementos_en_arbol_obtengo_arbol_con_elementos(){
   pa2m_afirmar(abb_insertar(arbol, cosa_tres), "Se puede insertar un tercer elemento correctamente.");
   pa2m_afirmar(arbol->nodo_raiz->derecha->elemento == cosa_tres, "Se inserta un segundo elemento en el lugar correspondiente.");
   pa2m_afirmar(abb_tamanio(arbol) == 3, "El tamanio del arbol es el correspondiente.");
-  pa2m_afirmar(!abb_vacio(arbol),"Un arbol con tres elementos no se encuentra vacio.");
+  pa2m_afirmar(!abb_vacio(arbol), "Un arbol con tres elementos no se encuentra vacio.");
   pa2m_afirmar(abb_insertar(arbol, NULL), "Se puede insertar un elemento nulo al arbol.");
   pa2m_afirmar(arbol->nodo_raiz->izquierda->izquierda->elemento == NULL, "El elemento es el correspondiente.");
   auxiliar->clave = 0;
   pa2m_afirmar(abb_buscar(arbol, auxiliar) == NULL, "Al buscar el elemento nulo, retorna NULL.");
   pa2m_afirmar(abb_tamanio(arbol) == 4, "El tamanio del arbol es el correspondiente.");
-  pa2m_afirmar(abb_insertar(arbol, cosa_cuatro) ,"Se puede insertar un cuarto elemento.");
-  pa2m_afirmar(abb_tamanio(arbol) == 5, "El tamanio del arbol es el correspondiente." );
+  pa2m_afirmar(abb_insertar(arbol, cosa_cuatro), "Se puede insertar un cuarto elemento.");
+  pa2m_afirmar(abb_tamanio(arbol) == 5, "El tamanio del arbol es el correspondiente.");
   pa2m_afirmar(arbol->nodo_raiz->izquierda->izquierda->izquierda->elemento == cosa_cuatro, "El elemento insertado es el correspondiente.");
   destruir_cosa(auxiliar);
   abb_destruir_todo(arbol, destructor);
 }
 
-//quitar & buscar
-void se_quitan_elementos_de_un_arbol_exitosamente(){
+// quitar y buscar
+void se_quitan_elementos_de_un_arbol_exitosamente()
+{
   abb_t *abb = abb_crear(comparador);
   cosa_t *cosa_uno = crear_cosa(10);
   cosa_t *cosa_dos = crear_cosa(5);
@@ -125,20 +136,20 @@ void se_quitan_elementos_de_un_arbol_exitosamente(){
   abb_insertar(abb, cosa_seis);
   abb_insertar(abb, cosa_siete);
 
-  pa2m_afirmar(abb!= NULL, "Se crea un nuevo arbol exitosamente.");
+  pa2m_afirmar(abb != NULL, "Se crea un nuevo arbol exitosamente.");
   pa2m_afirmar(abb_tamanio(abb) == 7, "El tamanio del arbol con 8 elementos es el correspondiente.");
   auxiliar->clave = 25;
   pa2m_afirmar(abb_buscar(abb, auxiliar) == cosa_siete, "Al buscar un elemento, retorna el correspondiente.");
   pa2m_afirmar(abb_quitar(abb, auxiliar) == cosa_siete, "Cuando se quita un nodo hoja el elemento quitado es el correspondiente.");
   pa2m_afirmar(abb_buscar(abb, auxiliar) == NULL, "Al buscar un quitado, retorna NULL.");
   auxiliar->clave = 5;
-   pa2m_afirmar(abb_tamanio(abb) == 6, "El tamanio del arbol ahora es 6.");
+  pa2m_afirmar(abb_tamanio(abb) == 6, "El tamanio del arbol ahora es 6.");
   pa2m_afirmar(abb_quitar(abb, auxiliar) == cosa_dos, "Cuando se quita un nodo con dos hijos, el elemento quitado es el correspondiente.");
   pa2m_afirmar(abb_tamanio(abb) == 5, "El tamanio del arbol ahora es 5.");
   auxiliar->clave = 20;
   pa2m_afirmar(abb_buscar(abb, auxiliar) == cosa_tres, "Al buscar un elemento, retorna el correspondiente.");
   pa2m_afirmar(abb_quitar(abb, auxiliar) == cosa_tres, "Se puede quitar un elemento que tiene dos nodos hijos.");
-  pa2m_afirmar(abb_tamanio(abb)== 4, "El tamanio luego de quitar un elemento es el correspondiente.");
+  pa2m_afirmar(abb_tamanio(abb) == 4, "El tamanio luego de quitar un elemento es el correspondiente.");
 
   destruir_cosa(cosa_dos);
   destruir_cosa(cosa_siete);
@@ -147,8 +158,9 @@ void se_quitan_elementos_de_un_arbol_exitosamente(){
   abb_destruir_todo(abb, destructor);
 }
 
-void se_buscan_elementos_dentro_de_un_arbol_obtengo_elementos_correspondientes(){
-abb_t *abb = abb_crear(comparador);
+void se_buscan_elementos_dentro_de_un_arbol_obtengo_elementos_correspondientes()
+{
+  abb_t *abb = abb_crear(comparador);
   cosa_t *cosa_uno = crear_cosa(10);
   cosa_t *cosa_dos = crear_cosa(5);
   cosa_t *cosa_tres = crear_cosa(20);
@@ -165,25 +177,26 @@ abb_t *abb = abb_crear(comparador);
   abb_insertar(abb, cosa_seis);
   abb_insertar(abb, cosa_siete);
 
-  pa2m_afirmar(abb!=NULL, "Se crea un arbol distinto correctamente.");
+  pa2m_afirmar(abb != NULL, "Se crea un arbol distinto correctamente.");
   pa2m_afirmar(abb_tamanio(abb) == 7, "El tamanio del arbol es el correspondiente.");
   pa2m_afirmar(abb_buscar(abb, cosa_seis) == cosa_seis, "Buscar el elemento 17, retorna 17.");
   pa2m_afirmar(abb_buscar(abb, abb->nodo_raiz->elemento) == cosa_uno, "Buscar el elemento de un nodo raiz devuelve el elemnto correspondiente.");
   pa2m_afirmar(abb_buscar(abb, cosa_inexistente) == NULL, "Al buscar un elemento que no existe dentro del arbol, no lo encuentra.");
-  
+
   destruir_cosa(cosa_inexistente);
   abb_destruir_todo(abb, destructor);
 }
 
-//con_cada_elemento
+// con_cada_elemento
 
-//siempre devuelve true.
-bool es_true(){
+// siempre devuelve true.
+bool es_true()
+{
   return true;
 }
 
-
-void pruebas_recorrido_con_cada_elemento_funcion_true_retornan_los_tres_recorridos_igual_cantidad(){
+void pruebas_recorrido_con_cada_elemento_funcion_true_retornan_los_tres_recorridos_igual_cantidad()
+{
   abb_t *abb = abb_crear(comparador);
   cosa_t *cosa_uno = crear_cosa(8);
   cosa_t *cosa_dos = crear_cosa(4);
@@ -206,28 +219,34 @@ void pruebas_recorrido_con_cada_elemento_funcion_true_retornan_los_tres_recorrid
   abb_destruir_todo(abb, destructor);
 }
 
-//Devuelve false si el elemento es igual a 4.
-bool hasta_cuatro(void *elemento, void *aux){
+// Devuelve false si el elemento es igual a 4.
+bool hasta_cuatro(void *elemento, void *aux)
+{
   aux = aux;
-  if(elemento){
-    if(((cosa_t *)elemento)->clave == 4){
+  if (elemento)
+  {
+    if (((cosa_t *)elemento)->clave == 4)
+    {
       return false;
     }
   }
   return true;
 }
 
-//Devuelve false si el elemento es igual a 10.
-bool hasta_diez(void *elemento, void *aux){
+// Devuelve false si el elemento es igual a 10.
+bool hasta_diez(void *elemento, void *aux)
+{
   aux = aux;
-  if(elemento){
-    if(((cosa_t *)elemento)->clave == 10)
+  if (elemento)
+  {
+    if (((cosa_t *)elemento)->clave == 10)
       return false;
   }
   return true;
 }
 
-void pruebas_recorrido_con_cada_elemento(){
+void pruebas_recorrido_con_cada_elemento()
+{
   abb_t *abb = abb_crear(comparador);
   cosa_t *cosa_uno = crear_cosa(8);
   cosa_t *cosa_dos = crear_cosa(4);
@@ -242,7 +261,6 @@ void pruebas_recorrido_con_cada_elemento(){
   abb_insertar(abb, cosa_cinco);
   abb_insertar(abb, cosa_seis);
 
-
   pa2m_afirmar(abb_con_cada_elemento(abb, INORDEN, hasta_cuatro, NULL) == 2, "La funcion dejo de recorrer correctamente para el recorrido inorden.");
   pa2m_afirmar(abb_con_cada_elemento(abb, PREORDEN, hasta_cuatro, NULL) == 2, "La funcion dejo de recorrer correctamente para el recorrido preorden.");
   pa2m_afirmar(abb_con_cada_elemento(abb, POSTORDEN, hasta_cuatro, NULL) == 3, "La funcion dejo de recorrer correctamente para el recorrido postorden.");
@@ -251,12 +269,10 @@ void pruebas_recorrido_con_cada_elemento(){
   pa2m_afirmar(abb_con_cada_elemento(abb, POSTORDEN, hasta_diez, NULL) == 4, "La funcion dejo de recorrer correctamente para el recorrido postorden.");
 
   abb_destruir_todo(abb, destructor);
-} 
+}
 
-
-//recorrer 
-//inorden
-void se_recorre_un_arbol_vacio_obtengo_cero_elementos(){
+void se_recorre_un_arbol_vacio_obtengo_cero_elementos()
+{
   abb_t *abb = abb_crear(comparador);
   void *array[TAMANIO_VECTOR_PRUEBA];
   size_t cantidad_recorrida_inorden = abb_recorrer(abb, INORDEN, array, TAMANIO_VECTOR_PRUEBA);
@@ -269,7 +285,8 @@ void se_recorre_un_arbol_vacio_obtengo_cero_elementos(){
   abb_destruir(abb);
 }
 
-void se_recorre_un_arbol_inorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes(){
+void se_recorre_un_arbol_inorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes()
+{
   abb_t *abb = abb_crear(comparador);
   void *array[TAMANIO_VECTOR_PRUEBA];
   cosa_t *diez = crear_cosa(10);
@@ -284,20 +301,20 @@ void se_recorre_un_arbol_inorden_con_elementos_obtengo_la_cantidad_de_elementos_
   abb_insertar(abb, nueve);
   abb_insertar(abb, diecisiete);
   abb_insertar(abb, quince);
-  
+
   size_t recorridos_inorden = abb_recorrer(abb, INORDEN, array, TAMANIO_VECTOR_PRUEBA);
   pa2m_afirmar(recorridos_inorden == TAMANIO_VECTOR_PRUEBA, "Recorrer el arbol INORDEN retorna la cantidad de elementos recorridos correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[0])== cuatro, "El primer elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[1])== siete, "El segundo elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[2])== nueve, "El tercero elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[3])== diez, "El cuarto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[4])== quince, "El quinto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[5])== diecisiete, "El sexto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[0]) == cuatro, "El primer elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[1]) == siete, "El segundo elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[2]) == nueve, "El tercero elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[3]) == diez, "El cuarto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[4]) == quince, "El quinto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[5]) == diecisiete, "El sexto elemento del array es el correspondiente.");
   abb_destruir_todo(abb, destructor);
-
 }
 
-void se_recorre_un_arbol_preorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes(){
+void se_recorre_un_arbol_preorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes()
+{
   abb_t *abb = abb_crear(comparador);
   void *array[TAMANIO_VECTOR_PRUEBA];
   cosa_t *diez = crear_cosa(10);
@@ -312,20 +329,20 @@ void se_recorre_un_arbol_preorden_con_elementos_obtengo_la_cantidad_de_elementos
   abb_insertar(abb, nueve);
   abb_insertar(abb, diecisiete);
   abb_insertar(abb, quince);
-  
+
   size_t recorridos_inorden = abb_recorrer(abb, PREORDEN, array, TAMANIO_VECTOR_PRUEBA);
   pa2m_afirmar(recorridos_inorden == TAMANIO_VECTOR_PRUEBA, "Recorrer el arbol PREORDEN retorna la cantidad de elementos recorridos correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[0])== diez, "El primer elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[1])== siete, "El segundo elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[2])== cuatro, "El tercero elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[3])== nueve, "El cuarto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[4])== diecisiete, "El quinto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[5])== quince, "El sexto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[0]) == diez, "El primer elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[1]) == siete, "El segundo elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[2]) == cuatro, "El tercero elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[3]) == nueve, "El cuarto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[4]) == diecisiete, "El quinto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[5]) == quince, "El sexto elemento del array es el correspondiente.");
   abb_destruir_todo(abb, destructor);
-
 }
 
-void se_recorre_un_arbol_postorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes(){
+void se_recorre_un_arbol_postorden_con_elementos_obtengo_la_cantidad_de_elementos_correspondientes()
+{
   abb_t *abb = abb_crear(comparador);
   void *array[TAMANIO_VECTOR_PRUEBA];
   cosa_t *diez = crear_cosa(10);
@@ -340,20 +357,20 @@ void se_recorre_un_arbol_postorden_con_elementos_obtengo_la_cantidad_de_elemento
   abb_insertar(abb, nueve);
   abb_insertar(abb, diecisiete);
   abb_insertar(abb, quince);
-  
+
   size_t recorridos_inorden = abb_recorrer(abb, POSTORDEN, array, TAMANIO_VECTOR_PRUEBA);
   pa2m_afirmar(recorridos_inorden == TAMANIO_VECTOR_PRUEBA, "Recorrer el arbol PREORDEN retorna la cantidad de elementos recorridos correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[0])== cuatro, "El primer elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[1])== nueve, "El segundo elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[2])== siete, "El tercero elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[3])== quince, "El cuarto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[4])== diecisiete, "El quinto elemento del array es el correspondiente.");
-  pa2m_afirmar((cosa_t *)(array[5])== diez, "El sexto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[0]) == cuatro, "El primer elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[1]) == nueve, "El segundo elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[2]) == siete, "El tercero elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[3]) == quince, "El cuarto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[4]) == diecisiete, "El quinto elemento del array es el correspondiente.");
+  pa2m_afirmar((cosa_t *)(array[5]) == diez, "El sexto elemento del array es el correspondiente.");
   abb_destruir_todo(abb, destructor);
-
 }
 
-int main(){
+int main()
+{
   pa2m_nuevo_grupo("PRUEBAS ABB");
   pa2m_nuevo_grupo("Pruebas NULL");
   pruebas_arbol_null();
